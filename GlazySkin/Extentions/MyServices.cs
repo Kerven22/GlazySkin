e449853c202c0f4c1bce7db30f0e52f35d16f2ea
Kerven Jumayev <kerwen.jumazew@gmail.com>
2025-04-23 21:36:31 +0300

    addin Service Extetntions class for Dependensy Injection

---
+++ GlazySkin/Extentions/MyServices.cs
@@ -0,0 +1,12 @@
+﻿using Entity;
+
+namespace GlazySkin.Extenttions
+{
+    public static class MyServices
+    {
+        public static void ConfigureSqlServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSqlServer<GlazySkinDbContext>(configuration.GetConnectionString("default"), b => b.MigrationsAssembly(nameof(GlazySkin))); 
+        }
+    }
+}
