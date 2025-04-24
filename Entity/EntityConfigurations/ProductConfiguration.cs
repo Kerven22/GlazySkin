eb2b52e19ff4e193b58ecd561e5ec4786f1929f3
Kerven Jumayev <kerwen.jumazew@gmail.com>
2025-04-24 07:49:38 +0300

    adding entity configuration folder

---
+++ Entity/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entity.EntityConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+          
+        }
+    }
+}
